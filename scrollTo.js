54cfd4499a92592c662e9e6393254f5acdb5a161
wy901015 <wy901015@gmail.com>
2014-12-31 01:20:10 +0800

    reverse for testing

---
+++ scrollTo.js
@@ -0,0 +1,12 @@
+        <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-scrollTo/1.4.14/jquery.scrollTo.min.js"></script>
+
+
+        
+        <script>
+            $(function() {
+                $(".rightclick").click(function() {
+                    $.scrollTo('#teamintro', 800);
+                });
+            });
+        </script>
